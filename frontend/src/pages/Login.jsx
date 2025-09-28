b6db41e82c7055b825a80f71f16ffeaf9e68fd9c
Jackie Ou <jackiehgou@gmail.com>
2025-09-28 16:42:42 -0700

    fix: add from fallback route

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,21 +1,20 @@
 import { useAuth } from "../context/AuthContext";
 import { Navigate, useLocation, useNavigate } from "react-router-dom";
 import { useHandleLogin } from "../utils/handleLogin";
+import { ROUTES } from "../constants/routes";
 
 export default function Login() {
   // Context
   const { user } = useAuth();
+  const login = useHandleLogin();
+  const userData = { name: "User 1" };
   // Router
   const location = useLocation();
   const navigate = useNavigate();
-  const from = location.state.from.pathname;
+  const from = location.state?.from?.pathname || ROUTES.HOME;
   if (user) {
     return <Navigate to={from} replace />
   }
-
-  // Context
-  const login = useHandleLogin();
-  const userData = { name: "User 1" };
 
   return (
     <div className="login-page">
